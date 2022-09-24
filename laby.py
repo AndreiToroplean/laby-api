f7ef88e80b7aa3aafa499e4da5f19877f16da26d
Andrei Toroplean <60916097+AndreiToroplean@users.noreply.github.com>
2022-09-24 15:22:57 +0200

    Improve names and adjust types in meths related to printing.

--- laby.py
+++ laby.py
@@ -1,5 +1,4 @@
 import enum
-import functools
 
 
 class Laby:
@@ -8,14 +7,14 @@ class Laby:
         n_rows, n_cols = size
         self.data = [[Node() for _ in range(n_cols)] for _ in range(n_rows)]
 
-    def __str__(self):
-        # TODO: improve var names.
-        lines = []
+    @property
+    def strs(self):
         for row_nodes in self.data:
-            for row_nodes_lines in zip(*[node.strs for node in row_nodes]):
-                lines_ = [''.join(cell_ls) for cell_ls in row_nodes_lines]
-                lines.append(''.join(lines_))
-        return '\n'.join(lines)
+            for strs in zip(*(node.strs for node in row_nodes)):
+                yield ''.join(strs)
+
+    def __str__(self):
+        return '\n'.join(self.strs)
 
 
 class Node:
@@ -29,18 +28,12 @@ class Node:
         self.dirs = dirs
 
     @property
-    @functools.cache
     def strs(self):
-        strs = [[
-                Chars.LRUD_CORNER,
-                Chars.EMPTY if Dir.UP in self.dirs else Chars.H_WALL,
-            ],
-            [
-                Chars.EMPTY if Dir.LEFT in self.dirs else Chars.V_WALL,
-                Chars.EMPTY,
-            ],
-        ]
-        return strs
+        yield f'{Chars.LRUD_CORNER}{Chars.EMPTY if Dir.UP in self.dirs else Chars.H_WALL}'
+        yield f'{Chars.EMPTY if Dir.LEFT in self.dirs else Chars.V_WALL}{Chars.EMPTY}'
+
+    def __str__(self):
+        return '\n'.join(self.strs)
 
 
 class Dir(enum.Enum):
