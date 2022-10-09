c01c8e5e832c0e98e193af40055f7fb8d783e834
Andrei Toroplean <60916097+AndreiToroplean@users.noreply.github.com>
2022-10-09 17:01:59 +0200

    Simplify `Grid.__setitem__`, `__getitem__`.
    Make names more consistent ("item").
    Name param "index" directly instead of key.
    Possibly fix some edge case bugs.

--- grid.py
+++ grid.py
@@ -26,31 +26,30 @@ class Grid(list):
     def __repr__(self):
         return f'{self.__class__.__name__}({super().__repr__()})'
 
-    def __setitem__(self, key: Sequence[int, ...] | int, value: _GridValue | Any):
+    def __setitem__(self, index: Sequence[int, ...] | int, item: _GridValue | Any):
         try:
-            index, *indices = key
+            index, *indices = index
         except TypeError:
-            if isinstance(value, Sequence):
-                value = self.__class__(value)
-            super().__setitem__(key, value)
-            return
+            indices = ()
+
+        if isinstance(item, Sequence):
+            item = self.__class__(item)
 
         if not indices:
-            super().__setitem__(index, value)
+            super().__setitem__(index, item)
             return
 
-        self.__getitem__(index).__setitem__(indices, value)
+        self.__getitem__(index).__setitem__(indices, item)
 
-    def __getitem__(self, key: Sequence[int | slice, ...] | int | slice) -> _GridValue | Any:
+    def __getitem__(self, index: Sequence[int | slice, ...] | int | slice) -> _GridValue | Any:
         try:
-            index, *indices = key
+            index, *indices = index
         except TypeError:
-            item = super().__getitem__(key)
-            if isinstance(key, slice):
-                item = self.__class__(item)
-            return item
+            indices = ()
 
-        item = self.__getitem__(index)
+        item = super().__getitem__(index)
+        if isinstance(index, slice):
+            item = self.__class__(item)
         if not indices:
             return item
 
