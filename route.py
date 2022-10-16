aa8b8042b878015ccc322edc41f8f73a033d4a28
Andrei Toroplean <60916097+AndreiToroplean@users.noreply.github.com>
2022-10-16 21:11:16 +0200

    Add `MultiRoute` which is a `Route` made of `Route`s.

--- route.py
+++ route.py
@@ -31,3 +31,54 @@ class Route:
             return self
 
         return self.prev.start
+
+
+class MultiRoute(Route):
+    def __init__(self, pos):
+        route = Route(pos)
+        self.routes = [route]
+
+    @property
+    def pos(self):
+        return self.routes[-1].pos
+
+    @pos.setter
+    def pos(self, pos):
+        self.routes[-1].pos = pos
+
+    @property
+    def next(self):
+        return self.routes[-1].next
+
+    @next.setter
+    def next(self, next_):
+        self.routes[-1].next = next_
+
+    @property
+    def next_dirs(self):
+        return self.routes[-1].next_dirs
+
+    @next_dirs.setter
+    def next_dirs(self, next_dirs):
+        self.routes[-1].next_dirs = next_dirs
+
+    @property
+    def prev(self):
+        return self.routes[-1].prev
+
+    @prev.setter
+    def prev(self, prev):
+        self.routes[-1].prev = prev
+
+    def __len__(self):
+        len_ = 0
+        for route in self.routes:
+            len_ += len(route)
+        return len_
+
+    @property
+    def all_poss(self):
+        all_poss = set()
+        for route in self.routes:
+            all_poss = all_poss.union(route.all_poss)
+        return all_poss
