828125f761f7dff2c4cdcc5591a767227bdae354
Andrei Toroplean <60916097+AndreiToroplean@users.noreply.github.com>
2022-10-16 20:06:27 +0200

    Extract `_get_dirs_choices` out of `_find_route`.

--- generate_and_solve.py
+++ generate_and_solve.py
@@ -2,7 +2,6 @@ from collections import namedtuple
 
 from laby import Laby
 from route import Route
-from utils import Dirs
 
 
 def generate_empty(shape):
@@ -20,23 +19,15 @@ def solve(laby):
 def _find_route(laby):
     route = Route(laby.start)
     while route.pos != laby.finish:
-        node = laby[route.pos]
-        possible_dirs = node.dirs.copy()
-        for prev_next in route.next:
-            possible_dirs &= ~prev_next.dir
-        possible_unvisited_dirs = Dirs.NONE
-        for dir_ in possible_dirs:
-            if route.pos + dir_ not in route.all_poss:
-                possible_unvisited_dirs |= dir_
-        possible_dirs = possible_unvisited_dirs
-        if not possible_dirs:
+        dirs_choices = _get_dirs_choices(laby, route)
+        if not dirs_choices:
             if route.pos == laby.start:
                 raise Exception('No solution!')
 
             route = route.prev
             continue
 
-        choice = possible_dirs.choice()
+        choice = dirs_choices.choice()
         next_route = Route(route.pos + choice)
         route.next.append(_Next(choice, next_route))
 
@@ -44,6 +35,17 @@ def _find_route(laby):
         route = next_route
         route.prev = prev_route
     return route
+
+
+def _get_dirs_choices(laby, route):
+    node = laby[route.pos]
+    dirs_choices = node.dirs.copy()
+    for prev_next in route.next:
+        dirs_choices &= ~prev_next.dir
+    for dir_ in dirs_choices.copy():
+        if route.pos + dir_ in route.all_poss:
+            dirs_choices &= ~dir_
+    return dirs_choices
 
 
 def _mark_route(laby, route):
