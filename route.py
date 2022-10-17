564a265dca3753399666fac0ced89ea23ee2a69c
Andrei Toroplean <60916097+AndreiToroplean@users.noreply.github.com>
2022-10-17 23:23:40 +0200

    Add Route.__len__.

--- route.py
+++ route.py
@@ -1,5 +1,17 @@
+from functools import cache
+
+
 class Route:
     def __init__(self, pos):
         self.pos = pos
         self.next = []
         self.prev = None
+
+    @cache
+    def __len__(self):
+        len_ = 0
+        route = self
+        while route is not None:
+            route = route.prev
+            len_ += 1
+        return len_
