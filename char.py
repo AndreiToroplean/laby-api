3c1b9eb2ab8e0724d6922d36d52e2b35413d2ed3
Andrei Toroplean <60916097+AndreiToroplean@users.noreply.github.com>
2022-10-24 00:21:54 +0200

    Add `_CharVariants` to be able to define variants of `Char`s.

--- char.py
+++ char.py
@@ -1,9 +1,31 @@
 from __future__ import annotations
 
+from collections.abc import Callable
+
 from dirs import Dirs
 
 
 _H_LEN = 4
+
+
+class _CharVariants(str):
+    def __new__(cls, value: str, bold: str = None):
+        instance = super().__new__(cls, value)
+        instance._bold = bold
+        return instance
+
+    @property
+    def bold(self) -> str:
+        if self._bold is not None:
+            return self._bold
+
+        return self
+
+    def transform(self, transformation: Callable[[str], str]) -> _CharVariants:
+        return self.__class__(
+            transformation(self),
+            transformation(self._bold),
+        )
 
 
 class Char:
